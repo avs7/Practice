edd233797d11c81d12e49add9a8a7e460ac3017f
Aleksandr <aslyusarev@gmail.com>
2022-08-04 15:45:59 -0700

    finished problem

--- script.js
+++ script.js
@@ -9,11 +9,15 @@ function sumAll(arr) {
   //fill array with first, last and in-between numbers
   let newArr = [];
   let i = sortArr[0];
-  while (i >= sortArr[0] && i <=sortArr[1]) {
+  while (i >= sortArr[0] && i <= sortArr[1]) {
     newArr.push(i);
     i++;
   }
-  return newArr;
+
+  //add all of the elements of newArr for final result
+  return newArr.reduce((total, num) => {
+    return total + num;
+  }, 0);
 }
 
-console.log(sumAll([23, 3]));
+console.log(sumAll([1, 3]));
