488ec2e241f9d65adf1cfe56bb096757fdcfa406
Aleksandr <aslyusarev@gmail.com>
2022-08-12 10:56:35 -0700

    work on fcc challenge

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 "use strict";
 
-function sumAll(arr) {
-  //sort array from smallest to largest
-  let sortArr = arr.sort(function (a, b) {
-    return a - b;
+function destroyer(arr, ...nums) {
+  arr.filter((value, index) => {
+    if (nums.includes(value)) {
+      arr.pop(index);
+    }
   });
-
-  //fill array with first, last and in-between numbers
-  let newArr = [];
-  let i = sortArr[0];
-  while (i >= sortArr[0] && i <= sortArr[1]) {
-    newArr.push(i);
-    i++;
-  }
-
-  //add all of the elements of newArr for final result
-  return newArr.reduce((total, num) => {
-    return total + num;
-  }, 0);
+  return arr;
 }
 
-console.log(sumAll([1, 3]));
+const result = destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+console.log(result);
