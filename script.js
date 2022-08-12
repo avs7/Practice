5f60f250670b33f4c2e1bef2c37555b05d9ff530
Aleksandr <aslyusarev@gmail.com>
2022-08-12 11:12:41 -0700

    solution

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 "use strict";
 
 function destroyer(arr, ...nums) {
-  arr.filter((value, index) => {
-    if (nums.includes(value)) {
-      arr.pop(index);
-    }
-  });
-  return arr;
+  return arr.filter((value) => !nums.includes(value));
 }
 
 const result = destroyer([1, 2, 3, 1, 2, 3], 2, 3);
